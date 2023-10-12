f817b5ff0d6b6647af86b1929b724d89fb7280b6
bjthompson17 <bjthom17@gmail.com>
2023-10-12 13:04:50 -0600

    Update example_program.py

--- TestTools/example_program.py
+++ TestTools/example_program.py
@@ -1,12 +1,23 @@
-"""This is an example program for the unit test template to use"""
+"""This is an example program for the example tests to use"""
+
+#pylint: disable = multiple-imports, wrong-import-position
 import time
+
+# This is here to make sure unit_tests module imports right because the
+# __init__.py isn't doing what I think it should be doing.
+import sys, os
+sys.path.append(os.path.dirname(__file__))
+import test_tools as unit_tests
+
+def empty():
+    """Function with no inputs or output"""
+    print("Empty function works.")
 
 def main():
     """Program entry point."""
     for _ in range(5):
         user_in = input("> ")
         print("Testing:", user_in)
-
 
 def long_function(some_input=None):
     """Function that simulates a task that takes too long"""
@@ -18,6 +29,23 @@ def long_function(some_input=None):
         time.sleep(1)
     return ("string", 1, True)
 
+def throw_error(error_text:str):
+    """Function that simulates an error being thrown"""
+    raise RuntimeError(error_text)
+
 
 if __name__ == "__main__":
-    main()
+    call_pack = unit_tests.UnitTestPack(long_function, "Hi").config(
+        user_input=range(1,20),
+        timeout=10,
+        print_input=False
+    )
+    try:
+        call_pack()
+    # pylint: disable = W0718
+    except Exception:
+        pass
+    else:
+        print("Return:", call_pack.rval)
+        print("\nstdout: \n" + call_pack.stdout)
+        print("\nstderr: \n" + call_pack.stderr)
